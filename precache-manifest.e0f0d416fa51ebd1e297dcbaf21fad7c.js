eadec44b9d538506b50ee5ac579104c41fb4b92a
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-16 22:27:03 +0400

    Updates

---
+++ precache-manifest.e0f0d416fa51ebd1e297dcbaf21fad7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ab8bf9d402ed3a3587947d4c4c20d3",
+    "url": "/bolt_/index.html"
+  },
+  {
+    "revision": "5709a78fe106facb5209",
+    "url": "/bolt_/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fa8755a0c9b0cdd5398",
+    "url": "/bolt_/static/js/2.307efc35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bolt_/static/js/2.307efc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5709a78fe106facb5209",
+    "url": "/bolt_/static/js/main.5cc8cfec.chunk.js"
+  },
+  {
+    "revision": "2bde08cdc9ec2f901b9d",
+    "url": "/bolt_/static/js/runtime-main.754bbb22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bolt_/static/media/logo.5d5d9eef.svg"
+  }
+]);
